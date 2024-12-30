b8e040447a61f81e4a955ef86bbf8ecf804d4679
Srulik P <srulikpinson770@gmail.com>
2024-12-30 09:43:02 -0700

    Full (GPT-generated) README

--- epub_to_txt.py
+++ epub_to_txt.py
@@ -6,7 +6,7 @@ def epub_to_txt(epub_path, txt_path):
     book = epub.read_epub(epub_path)
     with open(txt_path, 'w', encoding='utf-8') as txt_file:
         for item in book.get_items():
-            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            if item.get_type() == epub.ITEM_DOCUMENT:
                 soup = BeautifulSoup(item.get_content(), 'html.parser')
                 txt_file.write(soup.get_text())
 
